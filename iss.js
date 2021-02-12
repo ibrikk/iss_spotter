dd9fd55f446837438ea9764754a661df87dac04e
ibrikk <ibrahim.khalilov@yahoo.com>
2021-02-12 01:26:48 +0000

    calculating when ISS will fly over our location

--- iss.js
+++ iss.js
@@ -1,31 +1,38 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require('request');
 
-const fetchMyIP = (callback) => { 
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-  if (error) {
-    return callback(error, null);
-  }
-  if (response.statusCode !== 200) {
-    const msg = `Invalid status code: ${response.statusCode}. Response: ${body}`;
-    callback(Error(msg), null);
-    return;
-  }
+// ... other three functions not included in solution ...
 
-  const ipStr = (JSON.parse(body)).ip;
-  console.log(`my IP address is: ${ipStr}`);
-  callback(null, ipString);
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
   });
-  // use request to fetch IP address from JSON API
+};
 
-}
-
-module.exports = { fetchMyIP };
-
+// Only export nextISSTimesForMyLocation and not the other three (API request) functions.
+// This is because they are not needed by external modules.
+module.exports = { nextISSTimesForMyLocation };
